9da25cfe7024ea77fc1d7d93433646113ab21cf9
1NT18IS013 <1nt18is013.adithys@nmit.ac.in>
2021-12-14 10:53:06 +0530

    ADD : web view

---
+++ 14-12-21/Web/README.md
@@ -0,0 +1,7 @@
+# Web View
+
+- Create a web view to display a website using the WebView widget
+
+
+![screenshot](view.png)
+
---
+++ 14-12-21/Web/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView wv = findViewById(R.id.webView);
+
+        wv.loadUrl("https://www.chelseafc.com/en");
+
+        // enable javascript
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        wv.getSettings().setDomStorageEnabled(true);
+    }
+}
